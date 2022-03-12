5692e2cb68bdf41709e4e5b531ad55792439392b
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-12 05:21:51 -0500

    Added coin symbol, name, rank, price, and market cap to sidebar

--- src/components/Pages/CryptoPage.jsx
+++ src/components/Pages/CryptoPage.jsx
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Stack from '@mui/material/Stack'
+import { numberWithCommas } from '../CoinList/CoinList';
+import { LinearProgress } from '@mui/material';
 
 const CryptoPage = () => {
   const { id } = useParams();
@@ -19,8 +24,40 @@ const CryptoPage = () => {
     fetchCoin()
   }, [])
 
+  if(!coin) return <LinearProgress style={{ backgroundColor: '#0053ff' }} />
+
   return (
-	<div>CryptoPage</div>
+    <>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2} columns={16}>
+        {/* sidebar */}
+        <Grid item xs={12} md={6} lg={6}>
+          <div style={{ margin: 'auto', display: 'block' }}>
+          <img
+          src={coin?.image.large}
+          alt={coin?.name}
+          height='200'
+          />
+          <h1 style={{ margin: '20' }}>{coin?.name}</h1>
+          <h3>Rank: {coin?.market_cap_rank}</h3>
+          <h3>Current Price: ${numberWithCommas(coin?.market_data.current_price.usd)}</h3>
+          <h3>Market Cap: ${numberWithCommas(coin?.market_data.market_cap.usd)}</h3>
+          <h3>Genesis Date: {coin?.genesis_date}</h3>
+          </div>
+        </Grid>
+        <Grid item xs={8} lg={6}>
+        <Stack spacing={1}>
+          <Stack direction="row" spacing={1}>
+          </Stack>
+          <Stack direction="row" spacing={1}>
+          </Stack>
+        </Stack>
+        <p>{coin?.description.en.split(". ")[0]}</p>
+        {/* chart */}
+        </Grid>
+      </Grid>
+    </Box>
+    </>
   )
 }
 

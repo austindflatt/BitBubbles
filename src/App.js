6f114f02331b9658c4d78cacbc8cdc7c1f4985cb
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-11 22:06:42 -0500

    NavBar component is functional and routes are working correctly

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import CryptoPage from './components/Pages/CryptoPage';
-import HomePage from './components/Pages/HomePage';
-import AboutPage from './components/Pages/AboutPage';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import NavBar from './components/NavBar/NavBar'
+import CryptoPage from './components/Pages/CryptoPage'
+import HomePage from './components/Pages/HomePage'
+import AboutPage from './components/Pages/AboutPage'
+import CssBaseline from '@mui/material/CssBaseline'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 
-
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
 
 function App() {
   return (
     <Router>
+      <ThemeProvider theme={darkTheme}>
       <NavBar />
       <CssBaseline />
       <Box m={2} pt={3}>
@@ -20,13 +29,14 @@ function App() {
       {/* <AlertMessage /> */}
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/coins/:id' element={<CryptoPage />} />
+          <Route path='/crypto/:id' element={<CryptoPage />} />
           <Route path='/about' element={<AboutPage />} />
           <Route path='/404' element={null} />
           <Route path='/*' element={null} />
         </Routes>
       </Container>
       </Box>
+      </ThemeProvider>
     </Router>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,151 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const pages = ['About'];
+const settings = ['Watchlist', 'Logout'];
 
 const NavBar = () => {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-	<div>NavBar</div>
-  )
-}
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            component={Link}
+            to={'/'}
+            style={{ fontWeight: 'bold', color: '#2a96e9', cursor: 'pointer', textDecoration: 'none' }}
+          >
+            HODLSELL
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} component={Link} to={page.toLowerCase()}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component={Link}
+            to={'/'}
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            style={{ fontWeight: 'bold', color: '#2a96e9', cursor: 'pointer', textDecoration: 'none' }}
+          >
+            HODLSELL
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                component={Link}
+                to={page.toLowerCase()}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Austin Flatt" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem
+                key={setting}
+                component={Link}
+                to={setting.toLowerCase()}
+                >
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default NavBar

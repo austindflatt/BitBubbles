2c3e00f496174582b9f339ade55b55edaf0434a2
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-12 20:15:36 -0500

    fix: fixed package.json file for deployment

--- src/components/CoinInfo/CoinInfo.jsx
+++ src/components/CoinInfo/CoinInfo.jsx
@@ -1,22 +1,36 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { CircularProgress, Box } from '@mui/material'
+import { Line } from 'react-chartjs-2';
 
 const CoinInfo = ({ coin }) => {
   const [historicalData, setHistoricalData] = useState();
   const [days, setDays] = useState(1);
 
-  const fetchHistoricalData = async () =>{
+  const fetchHistoricalData = async () => {
     const { data } = await axios.get(`https://api.coingecko.com/api/v3/coins/${coin.id}/market_chart?vs_currency=USD&days=${days}`);
-
+    setHistoricalData(data.prices);
     console.log(data.prices)
   }
 
   useEffect(() => {
     fetchHistoricalData();
-  }, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [days])
+
 
   return (
     <>
+    {!historicalData ? (
+        <Box sx={{ display: 'flex' }} style={{width: '100px', margin: 'auto', display: 'block'}}>
+          <CircularProgress size={250} style={{ color: '#0053ff' }} />
+        </Box>
+      ) : (
+        <>
+        <p>chart will go here</p>
+        </>
+      )
+    }
     </>
   )
 }

45148e98222bcca7573ffbf33fade34889d6b3a3
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-15 21:33:35 -0400

    started working on charts, added different interval buttons and add/delete to watchlist

--- src/components/CoinInfo/CoinInfo.jsx
+++ src/components/CoinInfo/CoinInfo.jsx
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { CircularProgress, Box } from '@mui/material'
-import { Line } from 'react-chartjs-2';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { LineChart, AreaChart } from 'react-chartkick'
+import 'chartkick/chart.js'
+
 
 const CoinInfo = ({ coin }) => {
   const [historicalData, setHistoricalData] = useState();
@@ -27,7 +31,34 @@ const CoinInfo = ({ coin }) => {
         </Box>
       ) : (
         <>
-        <p>chart will go here</p>
+        <AreaChart 
+          data={[
+            historicalData.map((coin => {
+              let date = new Date(coin[0]);
+              let time =
+                date.getHours() > 12
+                  ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                  : `${date.getHours()}:${date.getMinutes()} AM`;
+
+                  return days === 1 ? time : date.toLocaleDateString()
+            })),
+          ]}
+          colors={["#0053ff"]}
+          
+        />
+        <br />
+        <Stack spacing={2} direction="row">
+          <Button variant="outlined">24 Hours</Button>
+          <Button variant="outlined">1 Month</Button>
+          <Button variant="outlined">3 Months</Button>
+          <Button variant="outlined">1 Year</Button>
+          <Button variant="outlined">All</Button>
+        </Stack>
+        <br />
+        <Stack spacing={2} direction="row">
+          <Button variant="outlined" color='success'>Add to watchlist</Button>
+          <Button variant="outlined" color='error'>Remove from watchlist</Button>
+        </Stack>
         </>
       )
     }
